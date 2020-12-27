33f29973ac2192af5bcf70517bad0554a51cc887
Kevin Buttimer <kbuttimer4@gmail.com>
2020-12-27 12:10:18 -0500

    updating final chart before enhancing

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let myChart1 = document.getElementById('myChart').getContext('2d');
-        
-            let chart1 = new Chart(myChart1, {
+let myChart = document.getElementById('myChart').getContext('2d');
+            let chart2 = document.getElementById('chart2').getContext('2d');
 
+            let chart1 = new Chart(myChart, {
+                
                 type:'bar', // bar, horizontalBar, pie, line, doughnut, etc
                 data:{
                     labels:['February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
@@ -44,3 +45,43 @@ let myChart1 = document.getElementById('myChart').getContext('2d');
                 }
                 
             });
+        
+
+               let casPopChart = new Chart(chart2, {
+                   type: 'bar',
+                   data:{
+                       labels:['February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                       datasets:[{
+                           label: 'Confirmed U.S. Deaths',
+                           data:[
+                               5,
+                               4332,
+                               59599,
+                               100780,
+                               120257,
+                               145513,
+                               175752,
+                               199084,
+                               222639,
+                               259697,
+                               314099,
+                           ],
+                           backgroundColor: 'red',
+                       }]
+                   },
+                   options: {
+                       title:{
+                           display:true,
+                           text: 'COVID Statistics',
+                           fontSize:25,
+                       },
+                    layout:{
+                        padding:{
+                            left:100,
+                            right:400,
+                            bottom:100,
+                            top:100,
+                        }
+                    }
+                   }
+               });
